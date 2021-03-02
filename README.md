08836bcea789f2f3eb16088a0aab7f8e76fd5d9a
chenj1901 <chenj1901@sany.com.cn>
2021-03-02 16:51:45 +0800

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # rl
 Reinforcement Learning
+
+Quick start
+-----------
+1. come into docker: `docker run -it -p8888:8888 -v /path/to/workdir:/app docker.io/maysnow/rl bash`
+2. launch jupyterlab: `xvfb-run --server-args="-screen 0, 1024x768x24" jupyter notebook --ip 0.0.0.0 --allow-root`
+3. copy address to web and start coding
--- tests/env_demo.py
+++ tests/env_demo.py
@@ -1,8 +1,15 @@
 import gym
+import matplotlib.pyplot as plt
 
-env = gym.make('CartPole-v0')
+env_name = 'CartPole-v0'
+
+plt.figure(figsize=(5,4))
+plt.title(env_name)
+
+env = gym.make(env_name)
 env.reset()
 for idx in range(1000):
-    env.render()
+    img = env.render(mode='rgb_array')
+    plt.imshow(img)
     env.step(env.action_space.sample())
 env.close()
